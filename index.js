1c0e55a9ba4c5abbe6efee8da374dd60d1c87069
mstyslav <mstyslav2010@gmail.com>
2024-04-19 17:13:05 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const age = 13
+const name = "Mstyslav"
+let isStudent = true
+let myString = "Неважно як повільно ти йдеш, головне - не зупиняйся"
+let myNumber = 7
+const myNull = null
+console.log(age, name, isStudent)
+console.log(myString)
+console.log(myNumber + 10)
+console.log(myNull)
+
+let yourName = prompt("enter your name")
+console.log("Привіт" + " " + yourName)
+
+let yesNo = confirm("Ви впевнені, що хочете продовжити?")
+if (yesNo) {
+    console.log("Дякую за підтвердження!")
+}
+else {
+    console.log("Дію відмінено!")
+}
+
+alert("Увага! Ця дія є небезпечною.");
+let yesNo1 = confirm("Ви впевнені, що хочете продовжити?")
+if (yesNo1) {
+    console.log("Дякую за підтвердження!")
+}
+else {
+    console.log("Дію відмінено!")
+}
+
+
+
+
+
