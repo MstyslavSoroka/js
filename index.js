fd280f66e16d78331ef4b81df9d02a253280228e
mstyslav <mstyslav2010@gmail.com>
2024-05-03 17:30:50 +0100

    hw3

--- index.js
+++ index.js
@@ -1,36 +1,9 @@
-let celsius = Number(prompt("enter temperarute in celsius"))
-let fahrenheit = (celsius * 9/5)+32;
-console.log(celsius + "°C" + " equals " + fahrenheit + "℉")
-// ===================================1============================================
+// let message = "Ваш баланс поповнено на 1"
+// let zeros = "000"
 
-let month = prompt("enter your month")
-let days = Number(prompt("how many days in your month?"))
-console.log(month+ " has " + days * 24 + " hours and " + days * 24 * 60 + " minutes")
-// ===================================2============================================
-let health = 100 - 50
-let energy = 100 - 20
+// let yourBalance = message+zeros
 
-console.log("Рівень здоров'я гравця: " + health);
-console.log("Рівень енергії гравця: " + energy);
-// ===================================3============================================
-let basket = 120 + 30 + 45 + 67 + 334 
-console.log(basket * 0.9)
-// ===================================4============================================
-let float = 43.785
-console.log(Math.floor(float))
-// ===================================5============================================
-let floatString = "23.42432246"
-console.log(parseFloat(floatString))
-// ===================================6============================================
-let numString = "123"
-console.log(parseInt(numString))
-// ===================================7============================================
-let rootNum = 64
-console.log(Math.sqrt(rootNum))
-// ===================================8============================================
-let numToStr = 12983
-let strToNum = "1476891"
-console.log(numToStr.toString())
-console.log(parseInt(strToNum))
+// console.log (yourBalance)
 
-// ===================================9============================================
+// console.log(yourBalance.length
+// )
