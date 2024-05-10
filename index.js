b557abd633a556cbeab69895146a2ec7a0d18c26
mstyslav <mstyslav2010@gmail.com>
2024-05-10 12:11:27 +0100

    hw4

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
-// let message = "Ваш баланс поповнено на 1"
-// let zeros = "000"
 
-// let yourBalance = message+zeros
 
-// console.log (yourBalance)
 
-// console.log(yourBalance.length
-// )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ==========================================================functions==========================================================
+function hw3() {
+    window.location.href = "hw3/index.html"
+}
+
+function hw4() {
+    window.location.href = "hw4/index.html"
+}
