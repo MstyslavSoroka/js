15aec80b8fe9a6cf6c9f6fbb5e9a5e52ff9e587c
mstyslav <mstyslav2010@gmail.com>
2024-05-31 18:36:33 +0100

    hw7

--- index.js
+++ index.js
@@ -14,5 +14,60 @@ function hw5() {
 function hw6() {
   window.location.href = "hw6/index.html";
 }
+
+function hw7() {
+  window.location.href = "hw7/index.html";
+}
 // ==========================================================functions==========================================================
+
+// const cart = [54, 28, 105, 70, 92, 17, 120, 12, 25, 90];
+
+// let total = 0;
+
+// for (const cost of cart) {
+//   total += cost;
+// }
+
+// console.log(total);
+
+// const Arr1 = [1, 5, "4", "hello"];
+// const Arr2 = [true, 2, {}, ["a"], 222];
+
+// console.log(Arr1[1]);
+// console.log(Arr2[1]);
+
+// console.log (Arr1[1] + Arr2[1])
+
+// Arr1[Arr1.length] = 22;
+
+// console.log (Arr1)
+
+// for (const item of Arr2) {
+//   console.log(`Значення: ${item}, Тип: ${typeof item}`);
+// }
+
+// const numbers = [1, 5, 8, 9, 12, 4, 15, 27, 30, 18, 14];
+
+// let total = 0;
+
+// for (const number of numbers) {
+//   if (number % 2 === 0) {
+//     total += number;
+//   }
+//   else {
+//     continue;
+//   }
+// }
+
+// console.log(total);
+
+const logins = ["user1", "user2", "user3", "user4"];
+
+if (logins.includes("user3")) {
+  console.log("знайдено.");
+} else {
+  console.log("не знайдено.");
+}
+
+
 
